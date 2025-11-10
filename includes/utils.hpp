f658848f160c41987ca842188f410cdd9b40f64d
clu <chuchulu117@gmail.com>
2025-11-10 16:05:26 +0200

    files added

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -1,13 +1,3 @@
 #pragma once
 
 #include <string>
-
-namespace utils {
-	inline std::string trimCRLF(const std::string& s) {
-		std::string out = s;
-		if (!out.empty() && out.back() == '\n') out.pop_back();
-		if (!out.empty() && out.back() == '\r') out.pop_back();
-		return out;
-	}
-}
-
