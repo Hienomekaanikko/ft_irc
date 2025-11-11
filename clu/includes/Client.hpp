d7227aa82dd1d5a3abcb047c38f83b5b3e0a0152
chuchulu29117 <chuchulu117@gmail.com>
2025-11-11 12:25:12 +0200

    updated client

--- clu/includes/Client.hpp
+++ clu/includes/Client.hpp
@@ -3,29 +3,59 @@
 #include <string>
 
 class Client {
+private:
+	int _fd = -1;
+	std::string _readBuffer;
+	std::string _writeBuffer;
+
+	// Identity & State
+	std::string _nickname;
+	std::string _username;
+	std::string _fullname;
+
+	bool _hasPassword = false;
+	bool _hasNickname = false;
+	bool _hasUsername = false;
+	bool _isRegistered = false;
+
 public:
 	Client() = default;
 	Client(int fd);
 
-	Client(const Client& other) = default;
-	Client& operator=(const Client& other) = default;
+	Client(const Client &other) = default;
+	Client &operator=(const Client &other) = default;
 
 	~Client() = default;
 
+	/// Getters ///
+	// File descriptor
 	int getFd() const noexcept;
+	
+	// Identity getters
+	const std::string &getNickname() const noexcept;
+	const std::string &getUsername() const noexcept;
+	const std::string &getFullname() const noexcept;
+
+	// Read line buffer
+	std::string &getReadBuffer() noexcept;
+	const std::string &getReadBuffer() const noexcept;
+
+	std::string &getWriteBuffer() noexcept;
+	const std::string &getWriteBuffer() const noexcept;
+
+	// Setters
 	void setFd(int fd) noexcept;
+	void setNickname(const std::string &nickname);
+	void setUsername(const std::string &username);
+	void setFullname(const std::string &fullname);
 
-	std::string& getReadBuffer() noexcept;
-	const std::string& getReadBuffer() const noexcept;
+	// Client state information
+	bool hasPassword() const noexcept;
+	bool hasNickname() const noexcept;
+	bool hasUsername() const noexcept;
+	bool isRegistered() const noexcept;
 
-	std::string& getWriteBuffer() noexcept;
-	const std::string& getWriteBuffer() const noexcept;
 
 	bool dataToWrite() const noexcept;
-	void queueMsg(const std::string& msg);
-
-private:
-	int			_fd = -1;
-	std::string _readBuffer;
-	std::string _writeBuffer;
+	void queueMsg(const std::string &msg);
 };
--- clu/src/Client.cpp
+++ clu/src/Client.cpp
@@ -3,17 +3,28 @@
 // Constructor
 Client::Client(int fd) : _fd(fd) {}
 
-// Getters and setters
+// Getters
 int Client::getFd() const noexcept
 {
 	return _fd;
 }
 
-void Client::setFd(int fd) noexcept
+const std::string& Client::getNickname() const noexcept
 {
-	_fd = fd;
+	return _nickname;
 }
 
+const std::string& Client::getUsername() const noexcept
+{
+	return _username;
+}
+
+const std::string& Client::getHostname() const noexcept
+{
+	return _hostname;
+}
+
+// Read and write buffers
 std::string& Client::getReadBuffer() noexcept
 {
 	return _readBuffer;
@@ -34,6 +45,49 @@ const std::string& Client::getWriteBuffer() const noexcept
 	return _writeBuffer;
 }
 
+// Setters
+void Client::setFd(int fd) noexcept
+{
+	_fd = fd;
+}
+
+void Client::setNickname(const std::string& nickname)
+{
+	_nickname = nickname;
+}
+
+void Client::setUsername(const std::string& username)
+{
+	_username = username;
+}
+
+void Client::setFullname(const std::string& fullname)
+{
+	_fullname = fullname;
+}
+
+// Client state information
+bool Client::hasPassword() const noexcept
+{
+	return _hasPassword;
+}
+
+bool Client::hasNickname() const noexcept
+{
+	return _hasNickname;
+}
+
+bool Client::hasUsername() const noexcept
+{
+	return _hasUsername;
+}
+
+bool Client::isRegistered() const noexcept
+{
+	return _isRegistered;
+}
+
+// Check if there is data to write
 bool Client::dataToWrite() const noexcept
 {
 	return !_writeBuffer.empty();
